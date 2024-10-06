9c3eebb543443da27d8a9529df1236c0c51ff746
Thirunavukkarasu T <97720002+thiru042004@users.noreply.github.com>
2024-10-07 00:33:25 +0530

    ADD - Added header for the photo component to include options to show when the images where selected.

--- src/components/PhotosComponent/PhotosComponent.jsx
+++ src/components/PhotosComponent/PhotosComponent.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect } from "react"
 import useImageContext from "../../hooks/useImageContext"
 import ImageGallery from "./ImageGallery"
-import { ArrowDownToLine, CopyPlus, Share2, X } from "lucide-react"
+import { ArrowDownToLine, CopyPlus, Share2, Trash2, X } from "lucide-react"
 
 const PhotosComponent = () => {
     const { globalImages, totalSelectedCount, groupImagesByDate, clearAllSelections } = useImageContext()
@@ -26,8 +26,93 @@ const PhotosComponent = () => {
                                 Gallery
                             </span>
                         :
-                            <div>
-                                {totalSelectedCount} selected
+                            <div
+                                className="w-full h-14 rounded-lg p-1 flex items-center"
+                                style={{boxShadow: "rgba(0, 0, 0, 0.24) 0px 3px 8px"}} 
+                            >
+                                <div>
+                                    <ul
+                                        className="flex items-center"
+                                    >
+                                        <li
+                                            className="flex items-center mx-1 px-3 py-2 rounded-md hover:bg-[#f0f0f0] hover:text-blue-600 hover:cursor-pointer hover:border-gray-600"
+                                        >
+                                            <span>
+                                                <Share2 
+                                                    size={18}
+                                                    strokeWidth={1}
+                                                />
+                                            </span>
+
+                                            <span
+                                                className="text-sm pl-2"
+                                            >
+                                                Share
+                                            </span>
+                                        </li>
+                                        <li
+                                            className="flex items-center mx-1 px-3 py-2 rounded-md hover:bg-[#f0f0f0] hover:text-blue-600 hover:cursor-pointer hover:border-gray-600"
+                                        >
+                                            <span>
+                                                <CopyPlus 
+                                                    size={18}
+                                                    strokeWidth={1}
+                                                />
+                                            </span>
+
+                                            <span
+                                                className="text-sm pl-2"
+                                            >
+                                                Add to Collection
+                                            </span>
+                                        </li>
+                                        <li
+                                            className="flex items-center mx-1 px-3 py-2 rounded-md hover:bg-[#f0f0f0] hover:text-blue-600 hover:cursor-pointer hover:border-gray-600"
+                                        >
+                                            <span>
+                                                <ArrowDownToLine 
+                                                    size={18}
+                                                    strokeWidth={1}
+                                                />
+                                            </span>
+
+                                            <span
+                                                className="text-sm pl-2"
+                                            >
+                                                Download
+                                            </span>
+                                        </li>
+                                        <li
+                                            className="flex items-center mx-1 px-3 py-2 rounded-md hover:bg-[#f0f0f0] hover:text-blue-600 hover:cursor-pointer hover:border-gray-600"
+                                        >
+                                            <span>
+                                                <Trash2 
+                                                    size={18}
+                                                    strokeWidth={1}
+                                                />
+                                            </span>
+
+                                            <span
+                                                className="text-sm pl-2"
+                                            >
+                                                Delete
+                                            </span>
+                                        </li>
+                                    </ul>
+                                </div>
+                                <div
+                                    className="ml-72"
+                                >
+                                    <button
+                                        className=" flex items-center justify-center text-sm cursor-pointer border border-gray-400 px-3 py-2 rounded-full hover:bg-[#f0f0f0] hover:text-blue-600 hover:border-gray-700"
+                                        onClick={clearAllSelections}
+                                    >
+                                        <span className="mr-1">
+                                            <X size={20} strokeWidth={1}/>
+                                        </span>
+                                        {totalSelectedCount} Selected
+                                    </button>
+                                </div>    
                             </div>
                 }
                 
